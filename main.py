f6b71e23fb282e26af9ca18aed1eb23004c2cbe1
Emiliano Galvan <tmxjuan.galvan@kof.com.mx>
2025-06-27 18:00:46 -0600

    my fisrt mcp-server package

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from mcp-new-env!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/mcpserver/__main__.py
@@ -0,0 +1,9 @@
+from mcpserver.deployment import mcp
+
+def main():
+    print("Starting MCP server...")
+    mcp.run()
+
+
+if __name__ == "__main__":
+    main()
