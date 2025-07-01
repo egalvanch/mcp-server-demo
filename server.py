45ca188e2fbb8769dcde8f8a8ffe20ae20848970
Emiliano Galvan <tmxjuan.galvan@kof.com.mx>
2025-06-30 20:06:28 -0600

    el servidor http esta configurado

---
+++ server.py
@@ -0,0 +1,11 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP(name="httpserver")
+
+@mcp.tool()
+def hello(name: str = "World") -> str:
+    """Returns a greeting message."""
+    return f"Hello, {name} from mcp-new-env!"
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
